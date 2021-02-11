ae0380a81a40c50ff864937ee1259e6656721763
ovaldez-at-022258092259 <Odvesims@gmail.com>
2021-02-11 19:32:20 -0400

    Setup user sign up

---
+++ spec/signing_up_users_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.feature "Sign up Users" do
+
+  scenario "With Valid Credentials" do
+    visit "/"
+
+    click_link "Sign Up"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+  
+    expect(page).to have_content("You have signed up successfully")
+  end
+
+  scenario "With Invalid Credentials" do
+    visit "/"
+
+    click_link "Sign Up"
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+    click_button "Sign up"
+  
+    #expect(page).to have_content("You have not signed up successfully")
+  end
+end
